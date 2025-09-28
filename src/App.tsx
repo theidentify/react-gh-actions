668555725dc7ecb1da8af31ac476849cbb565b6e
KrisanathepB <KrisanathepB@bedrockanalytics.ai>
2025-09-28 10:00:20 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Github Action Workshop Use Cache</h1>
+      <h1>Github Action Workshop Conditional</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

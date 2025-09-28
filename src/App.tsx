a6d54f5e9742daeeb92f0b90191d1a8b52d86d21
KrisanathepB <KrisanathepB@bedrockanalytics.ai>
2025-09-28 15:11:46 +0700

    something

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Github Action Workshop Deploy Watchtower</h1>
+      <h1>Github Action Workshop Deploy WatchTower</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
